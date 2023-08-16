b47b66ed6953a7fb2fe6c26b0469d7941bc99e0d
mogimogitomato <osakana.yano49@gmail.com>
2023-08-17 02:04:25 +0900

    mp3ファイル名を調整

--- get_jazz_tonight_mp3.rb
+++ get_jazz_tonight_mp3.rb
@@ -19,7 +19,7 @@ end
 def exec_command(url, date, title, comment)
   return if [url, date, title].any?(&:empty?)
 
-  file_name = "jazz_tonight_#{date}_#{title}".gsub(/ジャズ・トゥナイト　/, '').gsub(/\/|\\|\?|\*|<|>|\|/, '_')
+  file_name = "jazz_tonight_#{date}_#{title}"
   get_mp3_exec_command = %(ffmpeg -http_seekable 0 -i #{url} \
     -write_xing 0 -metadata title="#{date} #{title}" \
     -metadata artist="大友良英" -metadata album="ジャズ・トゥナイト" \
@@ -35,7 +35,10 @@ end
 file_list = fetch_file_list
 url = file_list[:file_name]
 date = file_list[:aa_vinfo3].slice(0, 8).gsub(/(\d{4})(\d{2})(\d{2})/, '\1-\2-\3')
-title = file_list[:file_title]
+title = file_list[:file_title].gsub(/ジャズ・トゥナイト/, '')
+                              .gsub(/[[:space:]]/, '')
+                              .gsub(/[Ａ-Ｚａ-ｚ０-９]/) { |s| http://s.tr('Ａ-Ｚａ-ｚ０-９ ', 'A-Za-z0-9') }
+                              .gsub(/\/|\\|\?|\*|<|>|\|/, '_')
 subscription = file_list[:file_title_sub]
 comment = "#{title}: #{subscription}"
 exec_command(url, date, title, comment)
